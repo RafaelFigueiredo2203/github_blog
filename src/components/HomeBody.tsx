a10dd9bb5595d182eb341d4dd97c20c811ca6eeb
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-12-18 16:38:29 -0300

    Corregindo erros de linting

--- src/components/HomeBody.tsx
+++ src/components/HomeBody.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios'
 import { useEffect, useMemo, useState } from 'react'
 import { useIssueContext } from '../utils/context/useIssueContext'
